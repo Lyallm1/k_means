52ef551cafd69c7aa1be03b258e4e8d9aad907e4
Lyallm1 <81273053+Lyallm1@users.noreply.github.com>
2022-06-29 00:54:15 +0000

    Modify

--- src/run.py
+++ src/run.py
@@ -6,7 +6,7 @@ def main(dataset_fn, output_fn, clusters_no):
     geo_locs = []
     for _, row in pd.read_csv(dataset_fn).iterrows(): geo_locs.append(Point(float(row['LAT']), float(row['LON'])))
     model = KMeans(geo_locs, clusters_no)
-    print("No of points are less than cluster number!") if model.fit(true) == -1 else model.save(output_fn)
+    print("No of points are less than cluster number!") if model.fit(True) == -1 else model.save(output_fn)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run k-means for location data", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
